63236bdaf7ecb3d5da8071da78d08595dd31832d
DjalmadeOliveira <101921463+Djalma-de-Oliveira@users.noreply.github.com>
2022-08-23 22:35:33 -0300

    ajuste netFlix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import Tmdb from './Tmdb'
+import Header from './components/Header'
 
-function App() {
+export default () => {
+  const [movvieList, setMovieList] = useState([]);
+  const [featureData, setFeatureData] = useState(null)
+  const [blackHeader, setBlackHeader] = useState(false)
+  
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+      console.log(list)
+      setMovieList(list)
+
+      let originals = list.filter(i => i.slug ==='originals')
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length-1))
+        let chosen = originals[0].items.results[randomChosen]
+        let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
+        console.log(chosenInfo)
+        setFeatureData(chosenInfo)
+      }
+        loadAll()
+   
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+        <Header black={blackHeader}
     </div>
-  );
+  )
 }
 
-export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import './Header.css'
+
+export default({ black }) => {
+    return {
+        <header className={black ? 'black' : ''}>
+        <div className="="header-logo>
+            <a href="/">
+                <img src="https://uploaad.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix logo"/>
+            </a>
+        </div>
+        <div className="="header-logo></div>
+    }
+}
