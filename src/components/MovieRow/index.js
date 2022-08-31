c10dc48e8c563360d798146221d38bbe667982ec
“Djalma-de-Oliveiragit config --global user.name “DjalmadeOliveira <djalma07031971@gmail.com>
2022-08-31 06:05:01 -0300

    commit 31/08

--- src/components/MovieRow/index.js
+++ src/components/MovieRow/index.js
@@ -1,45 +1,49 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 import './MovieRow.css'
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore'
+import NavigateNextIcon from '@mui/icons-material/NavigateNext'
+
 
 export default ({ title, items }) => {
-    const [scrollx, setScrolx] = useState(0);
+	const [scrollX, setScrollX] = useState(0)
 
-    const handLeftArrow = () => {
-        let x = scrollx + Math.round(window.innerWidth / 2)
-        if (x > 0) {
-            x = 0
-        }
-        setScrolx(x)
-    }
+	const handleLeftArrow = () => {
+		let x = scrollX + Math.round(window.innerWidth / 2)
+		if (x > 0) {
+			x = 0
+		}
+		setScrollX(x)
+	}
 
-    const handRightArrow = () => {
-        let x = scrollx - Math.round(window.innerWidth / 2)
-        let listW = items.results.length * 150
-        if ((window.innerWidth - listW) > x) {
-            x = (window.innerWidth - listW) - 60
-        }
-        setScrolx(x)
-    }
-    return (
-        <div className="=movieRow">
-            <h2>{title}</h2>
-            <div className="movieRow--left" onClick={handLeftArrow}>
-                <span style={{ fontSize: 50 }}> L </span>
-            </div>
-            <div className="movieRow--listarea">
-                <div className="movieRow--list" style={{
-                    marginLeft: scrollx,
-                    width: items.results.length * 150
-                }}>
-                    {items.results.length > 0 && items.results.map((item, key) => (
-                        <div className="movieRow--item" key={key}>
-                            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
-                                alt={item.original_title} />
-                        </div>
-                    ))}
-                </div>
-            </div>
-        </div>
-    )
+	const handleRightArrow = () => {
+		let x = scrollX - Math.round(window.innerWidth / 2)
+		let listW = items.results.length * 150
+		if ((window.innerWidth - listW) > x) {
+			x = (window.innerWidth - listW) - 60
+		}
+	}
+
+	return (
+		<div className="movieRow">
+			<h2>{title}</h2>
+			<div className="movieRow--left" onClick={handleLeftArrow}>
+				<NavigateBeforeIcon style={{fontsize: 50}} />
+			</div>
+			<div className="movieRow--right" onClick={handleRightArrow}>
+				<NavigateNextIcon style={{fontsize: 50}} />
+			</div>
+			<div className="movieRow--listarea">
+				<div className="movieRow--list" style={{
+					marginLeft: scrollX,
+					width: items.results.length * 150
+				}}>
+					{items.results.length > 0 && items.results.map((item, key) => (
+						<div className="movieRow--item" key={key}>
+							<img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	)
 }
-
